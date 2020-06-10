b10eff3359464ba61b57f1830781e0bbabc8cf0c
Justin Lundy <justindlundy@gmail.com>
2020-06-10 09:48:54 -0700

    Updating folder structure

--- graphql/modules/middleware/logger.ts
+++ graphql/modules/middleware/logger.ts
@@ -1,5 +1,5 @@
 import { MiddlewareFn } from "type-graphql";
-import { MyContext } from "src/types/MyContext";
+import { MyContext } from "graphql/types/MyContext";
 
 export const logger: MiddlewareFn<MyContext> = async ({ args }, next) => {
   console.log("args" + args);
