46a2cdef4f3558166a32babf1074097c9188ae0a
ssousa33 <ssousa2834@gmail.com>
2020-06-15 16:14:22 -0600

    adding user entity

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import ApolloClient from 'apollo-boost';
 import App from './App';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
+
+fetch('http://localhost:4000/graphql')
+.then(res=>{
+  console.log(res);
+})
 
 
 
