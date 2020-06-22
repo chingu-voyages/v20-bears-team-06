4f99e33d4f30f065c7a662923e3f0a2e1bd3332a
Justin Lundy <justindlundy@gmail.com>
2020-06-22 12:03:00 -0700

    adding path to middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -49,7 +49,7 @@ const main = async () => {
     })
   );
 
-  apolloServer.applyMiddleware({ app, cors: false });
+  apolloServer.applyMiddleware({ app, path: "/", cors: false });
 
   const PORT = process.env.PORT || 4000;
   console.log("server started on http://localhost:4000/graphql");
