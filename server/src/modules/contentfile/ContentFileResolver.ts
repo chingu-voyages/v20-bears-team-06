36f6a21a59fd7cd4f2b553703d0117582172d2e9
Justin Lundy <justindlundy@gmail.com>
2020-07-08 09:17:52 -0700

    reverting contentFileResolver to previous state

--- server/src/modules/contentfile/ContentFileResolver.ts
+++ server/src/modules/contentfile/ContentFileResolver.ts
@@ -29,8 +29,8 @@ export class NewFileArgs {
   @Field(() => ID)
   userId: number;
 
-    @Field({nullable:true})
-    signedRequest: string;
+  @Field({ nullable: true })
+  userUrl: string;
 
   @Field({ nullable: true })
   filetype: string;
@@ -38,9 +38,8 @@ export class NewFileArgs {
   @Field({ nullable: true })
   filename: string;
 
-    @Field({nullable:true})
-    key: string;
-
+  @Field({ nullable: true })
+  contentUrl: string;
 
   @Field({ nullable: true })
   key: string;
@@ -69,73 +68,77 @@ export class ContentFileResolver {
       return user.uploads ? user.uploads : [];
     }
 
-    @Mutation(() => ContentFile)
-    async newFile(
-        @Args() {userId, filename, filetype, signedRequest, key}: NewFileArgs,
-        @PubSub() pubSub: PubSubEngine
-    ): Promise<ContentFile|null>{
-        
-        
+    return [];
+  }
 
-        let file = await User.addNewFile({
-            userId,
-            signedRequest,
-            filename,
-            filetype,
-            key
+  @Query(() => [ContentFile])
+  async searchFiles(
+    @Arg("searchTerm") searchTerm: string
+  ): Promise<ContentFile[] | undefined> {
+    return ContentFile.find({
+      where: [{ filename: Like(`%${searchTerm}%`) }],
+    });
+  }
 
-        });
+  @Mutation(() => ContentFile)
+  async newFile(
+    @Args() { userId, filename, filetype, key, signedRequest }: NewFileArgs,
+    @PubSub() pubSub: PubSubEngine
+  ): Promise<ContentFile | null> {
+    let file = await User.addNewFile({
+      userId: userId,
+      //   userUrl: `/profile/${userId}`,
+      //   contentUrl,
+      key,
+      signedRequest,
+      filename,
+      filetype,
+    });
 
-        
-        const fileNotification = await User.addNotification({
-            type: NotificationType.FollowingUpload,
-            message: NotificationMessage.FollowingUpload,
-            url: `/profile/${userId}`,
-            toFollowers: true,
-            userId: userId,
-            fromUserId: userId,
-            fromUserName: await User.getName(userId)
+    const fileNotification = await User.addNotification({
+      type: NotificationType.FollowingUpload,
+      message: NotificationMessage.FollowingUpload,
+      url: `/profile/${userId}`,
+      toFollowers: true,
+      userId: userId,
+      fromUserId: userId,
+      fromUserName: await User.getName(userId),
+    });
 
-        });
-       
-        const payload: FilesPayload = {
-            userId: userId
-        };
+    const payload: FilesPayload = {
+      userId: userId,
+    };
 
-        if (fileNotification instanceof ToFollowerNotification){
-            let owners = await fileNotification.owners;
-            const toFollowerPayload: AddToFollowerPayload= new AddToFollowerPayload();
-            toFollowerPayload.ownerIds = owners.map(el=>el.id);
-            pubSub.publish(Topic.NewNotification, toFollowerPayload);
-            };
-
-            
-        
-        
-        pubSub.publish(Topic.NewFile,payload);
-        return file || null; 
+    if (fileNotification instanceof ToFollowerNotification) {
+      let owners = await fileNotification.owners;
+      const toFollowerPayload: AddToFollowerPayload = new AddToFollowerPayload();
+      toFollowerPayload.ownerIds = owners.map((el) => el.id);
+      pubSub.publish(Topic.NewNotification, toFollowerPayload);
     }
 
-        
-    
+    pubSub.publish(Topic.NewFile, payload);
+    return file || null;
+  }
 
-    @Subscription(() => [ContentFile], {nullable:true,
-        topics: Topic.NewFile,
-        filter: ( { payload, args }: ResolverFilterData<FilesPayload , FilesArgs>) => {
-            return payload.userId == args.userId;
-        },
-    })
-    async fileSub(
-        @Root() filesPayload: FilesPayload,
-        @Args() {userId}: FilesArgs
-    ) : Promise<ContentFile[]|[]>{
-        
-        let files = await ContentFile.createQueryBuilder('file')
-        .where('file.ownerId = :userId',{userId})
-        .getMany();
+  @Subscription(() => [ContentFile], {
+    nullable: true,
+    topics: Topic.NewFile,
+    filter: ({
+      payload,
+      args,
+    }: ResolverFilterData<FilesPayload, FilesArgs>) => {
+      return payload.userId == args.userId;
+    },
+  })
+  async fileSub(
+    // @Root() filesPayload: FilesPayload,
+    @Args() { userId }: FilesArgs
+  ): Promise<ContentFile[] | []> {
+    let files = await ContentFile.createQueryBuilder("file")
+      .where("file.ownerId = :userId", { userId })
+      .getMany();
 
-        
-        return files || [];
-    }
-    
+    console.log(typeof files);
+    return files || [];
+  }
 }
